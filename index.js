813100511a2b2449ba9ffbfb613ca6aaa414421d
anne belakonis <anne.belakonis@libertymutual.com>
2017-09-27 08:36:57 -0400

    First Commit
    HTML and CSS started

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// load manifests
+// scripts
+require('./assets/scripts/index.js')
+
+// styles
+require('./assets/styles/index.scss')
+
+// function startGame (){
+//   user.turn = "x";
+//   setMessageUser.turn+ "gets to start" )
+// }
+// function setMessa
